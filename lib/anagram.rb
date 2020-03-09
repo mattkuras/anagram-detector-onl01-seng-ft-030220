2425e60a1d10a130582426094b2cc31001abdfc7
Matt Kuras <mattkuras@gmail.com>
2020-03-09 21:24:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    split_word = @word.split("")
+    array_of_split_words = []
+    final_array = []
+
+    array.each do |element|
+    if element.split("").sort == split_word.sort 
+      final_array << element
+  end
+end
+  final_array
+end
+  
+end
