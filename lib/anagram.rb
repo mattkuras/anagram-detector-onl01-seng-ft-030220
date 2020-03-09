30cd2d417d45172de9b9ca0c1d24045b4b3d17b2
Matt Kuras <mattkuras@gmail.com>
2020-03-09 20:57:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,8 +14,9 @@ class Anagram
     final_array = []
 
     array.each do |element| array_of_split_words << element.split("")
-     binding.pry 
      array_of_split_words.each do |letters| 
+    binding.pry 
+
     if letters == split_word
       final_array << element
     
