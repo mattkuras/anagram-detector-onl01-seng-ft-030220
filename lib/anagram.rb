98d2656c99c342ffd650028f4877c42878eee889
Matt Kuras <mattkuras@gmail.com>
2020-03-09 20:43:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    split_word = @word.split("")
+    array_of_split_words = []
+    array.each do |element| array_of_split_words << element.split("")
+ 
+     array_of_split_words.each do |letters| 
+    if letters == split_word
+      element
+    end
+    binding.pry 
+  end
+  end
+end
+  
+end
